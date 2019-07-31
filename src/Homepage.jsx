05ec115141461b56c5e69d9599293374c776ebf7
TornikeIarajuli <tornikeiarajuli@gmail.com>
2019-07-31 20:29:16 +0400

    "Init commit"

---
+++ src/Homepage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./homepage.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory_menu">
+      <div className="menu_item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu_item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu_item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu_item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+
+      <div className="menu_item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
